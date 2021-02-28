2c17da95a2e0c05523aa231b25503eda5724c40a
Joseph McGeever <mcgeevej@uni.coventry.ac.uk>
2021-02-28 16:00:27 +0000

    pagination works

--- schedule/schedule/src/main/java/com/example/demo/UserBaseRepository.java
+++ schedule/schedule/src/main/java/com/example/demo/UserBaseRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public interface UserBaseRepository<T extends User> extends JpaRepository<T, String> {
 
-    public T findByDtype(String mode); //dtype - attribute in the User table for FTUser or PTUser
+    public T findByDtype(String mode); //dtype - attribute in the User table for trainee or learner
     public T findByUsername(String mode); //used for loggin in
     public void deleteByUsername(String username); //username - primary key in the User table
 
